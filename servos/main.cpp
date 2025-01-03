e59cea84bb0434c6bc291810294a9b514a23d0c3
Tom Jacobs <hatsmagee@gmail.com>
2025-01-03 03:19:36 -0500

    nice

--- servos/main.cpp
+++ servos/main.cpp
@@ -2,38 +2,36 @@
 #include "SMS_STS.h"
 
 int main() {
-	// Open serial
+    // Open serial
     std::string port_name = "/dev/ttyAMA0";
     SerialPort serial(port_name);
     if (!serial.openPort()) return 1;
+    SMS_STS st;
+    st.pSerial = &serial;
 
-    // Open ST servo driver
-	SMS_STS st;
-	st.pSerial = &serial;
+    int x = 0;
+    int y = 1500;
 
-int x = 0;
-int y = 1500;
+    do {
+        // Update
+        x += 10;
+        y += 10;
+        if (x > 2000) x = 0;
+        if (y > 2000) y = 1500;
 
-do 
-{
+        // Move
+        st.WritePosEx(1, x, 300, 10);
+        st.WritePosEx(2, y, 300, 10);
 
- x += 1;
- y += 1;
- if (x > 2000) x = 0;
- if (y > 2000) y = 1500;
-
-	// Move
-	st.WritePosEx(1, x, 300, 10);
-	st.WritePosEx(2, y, 300, 10);
-
-	int pos1 = st.ReadPos(1);
+        // Read
+        int pos1 = st.ReadPos(1);
 	printf("Pos1: %d\n", pos1);
 
-usleep(10000);
+        // Sleep
+        usleep(10000);
+    } while (true);
 
-} while (true);
-
-	return 0;
+    return 0;
 }
 
 
