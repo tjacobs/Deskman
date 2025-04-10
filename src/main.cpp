edcb44f9e89d8581b296ae7ea221d12f257d7a74
Tom Jacobs <hatsmagee@gmail.com>
2025-04-10 12:20:09 -0700

    Try SSH movement

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,11 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
+
 using namespace std;
 
-// Functions
 void look(bool right);
-void enable_raw_mode();
-void disable_raw_mode();
-void read_arrow_keys();
 
-// Main
 int main(int argc, char **argv) {
     // Connect to servos
     if (open_servos() != 0) printf("Could not open servos\n");
@@ -50,6 +43,7 @@ int main(int argc, char **argv) {
             static bool right = false;
             if (t > 30) {
                 t = 0;
+
 //                look(right);
 //                right = !right;
 
@@ -65,8 +59,6 @@ int main(int argc, char **argv) {
         }
     });
     movement_thread.detach();
-
-    //enable_raw_mode();
 
     // Process keyboard input on main thread
     SDL_Event event;
@@ -90,8 +82,26 @@ int main(int argc, char **argv) {
             }
         }
 
-        // Read arrow keys from terminal
-        //read_arrow_keys();
+        // Check for keyboard input from stdin
+        if (cin.rdbuf()->in_avail()) {
+            char input = cin.get();
+            switch(input) {
+                case 'A': // Up arrow
+                    move_head(0, 40); 
+                    update_face(&face, 0, 1);
+                    break;
+                case 'B': // Down arrow
+                    move_head(0, -40);
+                    update_face(&face, 0, -1);
+                    break;
+                case 'C': // Right arrow
+                    move_head(-40, 0);
+                    break;
+                case 'D': // Left arrow
+                    move_head(40, 0);
+                    break;
+            }
+        }
 
         // Clear the screen
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -109,7 +119,6 @@ int main(int argc, char **argv) {
 
     // Done
     close_window();
-    disable_raw_mode();
     return 0;
 }
 
@@ -124,47 +133,5 @@ void look(bool right) {
     move_head((right ? 1 : -1) * -800, 0);
     this_thread::sleep_for(chrono::milliseconds(1000));
     move_head(0, -100);
-}
-
-void enable_raw_mode() {
-    struct termios tty;
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag &= ~(ICANON); // | ECHO); // Disable canonical mode and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-    // Set stdin to non-blocking mode
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-}
-
-void disable_raw_mode() {
-    struct termios tty;
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag |= (ICANON | ECHO); // Re-enable canonical mode and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-    // Restore stdin to blocking mode
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags & ~O_NONBLOCK);
-}
-
-void read_arrow_keys() {
-    char ch;
-    ch = getchar();
-    if (ch == '\x1B') { // Start of escape sequence
-            char seq[2];
-            if (read(STDIN_FILENO, &seq[0], 1) > 0 &&
-                read(STDIN_FILENO, &seq[1], 1) > 0) {
-                if (seq[0] == '[') {
-                    switch (seq[1]) {
-                        case 'A': std::cout << "Up arrow pressed\n"; break;
-                        case 'B': std::cout << "Down arrow pressed\n"; break;
-                        case 'C': std::cout << "Right arrow pressed\n"; break;
-                        case 'D': std::cout << "Left arrow pressed\n"; break;
-                    }
-                }
-            }
-    } else if (ch > 0) {
-            std::cout << "Key pressed: " << ch << "\n";
-    }
-    if (ch == 'q') exit(0);
 }
 
